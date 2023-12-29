4b9c4dee8adeedd2fc7fedd7954d698b8ea82f90
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2023-12-29 10:17:56 +0300

    add lessons 5 and 6

---
+++ lesson_5/find.py
@@ -0,0 +1,24 @@
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+#
+# service = Service(executable_path=ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service)
+#
+# driver.get('https://www.freeconferencecall.com/login')
+#
+# driver.find_element(By.ID, 'loginformsubmit').click()
+
+
+
+
+# import By:
+# ID = "id"
+# XPATH = "xpath"
+# LINK_TEXT = "link text"
+# PARTIAL_LINK_TEXT = "partial link text"
+# NAME = "name"
+# TAG_NAME = "tag name"
+# CLASS_NAME = "class name"
+# CSS_SELECTOR = "css selector"
---
+++ lesson_6/find_elements_practice.py
@@ -0,0 +1,37 @@
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+#
+# service = Service(executable_path=ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service)
+#
+# driver.get('https://testautomationpractice.blogspot.com/')
+#
+# driver.find_element('class name', 'wikipedia-icon')
+# print('first OK')
+#
+# driver.find_element('id', 'Wikipedia1_wikipedia-search-input')
+# print('second OK')
+#
+# driver.find_element('class name', 'wikipedia-search-button')
+# print('third OK')
+#
+# driver.find_element('tag name', 'h1')
+# print('forth OK')
+
+
+
+
+
+
+
+
+# print(len(driver.find_elements('class name', 'nav-link')))
+#
+# driver.find_elements('class name', 'nav-link')[2].click()  # обращение к конкретному локатору в списке
+
+
+
+
+
